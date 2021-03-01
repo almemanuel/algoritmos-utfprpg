a65f557a7a26f84265c8978800016535ad0d4659
Emanuel Almeida <40006289+almemanuel@users.noreply.github.com>
2021-03-01 11:56:43 -0300

    On branch main 	new file:   atividades/0301-modified-hello-world.c

---
+++ atividades/0301-modified-hello-world.c
@@ -0,0 +1,15 @@
+/*
+Autor: Emanuel de Almeida Souza
+Data de criação: 01 de março de 2021
+E-mail: emanuel.1998@alunos.utfpr.edu.br
+
+Este algoritmo exibirá uma mensagem na tela.
+*/
+
+#include <stdio.h>
+
+int main() {
+	printf("Ola, mundo");
+	printf("\nEmanuel");
+	return 0;
+}

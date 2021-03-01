1f2446e5e834bdea6818b142cb61d481860bbb41
Emanuel Almeida <40006289+almemanuel@users.noreply.github.com>
2021-03-01 12:07:46 -0300

    On branch main 	new file:   0301-exercises-one-and-two.c

---
+++ atividades/0301-exercises-one-and-two.c
@@ -0,0 +1,19 @@
+/*
+1. Faça um programa que escreva a frase "Linguagem de Programacao
+Estruturada 2020-2", fazendo com que cada palavra seja impressa em
+uma linha separada.
+
+2. Comente cada linha do código anterior explicando sua função.
+*/
+
+#include <stdio.h> // adicionada a biblioteca padrão
+
+
+int main() { // principal subrotina do algoritmo
+	printf("Linguagem"); // imprime a palavra
+	printf("\nde"); // imprime a palavra na linha de baixo
+	printf("\nProgramacao"); // também imprime na linha de baixo
+	printf("\nEstruturada"); // aqui também
+	printf("\n2020-2"); // e aqui também
+	return 0; // retorna 0 para esta subrotina
+}

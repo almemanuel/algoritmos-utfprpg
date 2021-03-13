845ee6ad8fdb4c5c8f2470bb3cea7fd5b7eea652
Emanuel Almeida <40006289+almemanuel@users.noreply.github.com>
2021-03-13 01:29:30 -0300

    Update 0301-exercises-one-and-two.c

--- atividades/0301-exercises-one-and-two.c
+++ atividades/0301-exercises-one-and-two.c
@@ -16,7 +16,7 @@ int main() { // principal subrotina do algoritmo
 	printf("\nProgramacao"); // também imprime na linha de baixo
 	printf("\nEstruturada"); // aqui também
 	printf("\n2020-2"); // e aqui também
-	return 0; // retorna 0 para esta subrotina
+	//return 0; // retorna 0 para esta subrotina
 }
 
 // RM 11:36
